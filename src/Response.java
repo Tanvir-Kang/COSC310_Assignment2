7a8e8bc48cae65ba8247df02dfc547507face773
Tanvir-Kang <38304277+Tanvir-Kang@users.noreply.github.com>
2020-03-10 11:19:42 -0700

    Created greeting function, and sample greeting text

--- src/Response.java
+++ src/Response.java
@@ -1,4 +1,27 @@
-
+import java.io.*;
 public class Response {
+	
+	public String greeting() {
+		String greetingSentence="";
+		String s;
+		try {
+			File file = new File("src/greeting.txt");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while( (s = br.readLine())!=null) {
+				greetingSentence+=s;
+			}
+			System.out.println(greetingSentence);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+				System.out.println("Cannot find greeting file specified");
+				
+			}
 
+			
+		return null;
+		
+	}
+	
+	
 }
--- src/main.java
+++ src/main.java
@@ -1,8 +1,11 @@
 
-public class main {
+public class main extends Response {
 
 	public static void main(String[] args) {
+		Response r = new Response();
+		System.out.println("Greetings user, please take a look at the following options to talk about,");
 		
+		r.greeting();
 	}
 
 }
